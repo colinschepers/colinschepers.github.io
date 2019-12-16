cba1321d46edfe11ed873b9e0029db12ae1796d9
Colin Schepers <colinschepers@hotmail.com>
2019-12-17 00:55:23 +0100

    Add about section to website

--- js/main.js
+++ js/main.js
@@ -6,12 +6,28 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
+	$('.about').hide()
+	$('.main-post-list').show()
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      //$('.content-wrapper').addClass('animated slideInRight')
     } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
+  
+  $('a.about-button').click(function (e) {
+	$('.main-post-list').hide()
+    $('.about').show()
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+	currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      //$('.content-wrapper').addClass('animated slideInRight')
+	} else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
@@ -19,6 +35,14 @@ $(document).ready(function () {
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+	$('.about').hide()
+	$('.main-post-list').show()
+  }
+    
+  if (window.location.hash && window.location.hash == '#about') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+	$('.main-post-list').hide()
+	$('.about').show()
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
@@ -31,6 +55,11 @@ $(document).ready(function () {
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+  
+  $('.navigation-wrapper .about-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
